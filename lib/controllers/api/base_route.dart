c11d8c28fa22b3431acd228fb62be4a6bc9b41fe
Pradeep Kumar <4gpradeep@gmail.com>
2021-11-30 22:14:55 +0530

    base url

--- lib/controllers/api/base_route.dart
+++ lib/controllers/api/base_route.dart
@@ -1,4 +1,5 @@
 class BaseRoute {
   // static const String domain = 'http://127.0.0.1:8000';
-  static const String domain = 'http://psym.herokuapp.com';
+  // static const String domain = 'http://psym.herokuapp.com';
+  static const String domain = 'http://psymenv7.eba-7gripvhk.us-west-2.elasticbeanstalk.com/';
 }
